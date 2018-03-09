cd301e79706c86ddcc03f9dc88a9764bb2477fb0
vurosevic <35973264+vurosevic@users.noreply.github.com>
2018-03-09 15:24:16 +0100

    start

--- src/predictions/neuralnetwork.clj
+++ src/predictions/neuralnetwork.clj
@@ -3,6 +3,17 @@
             [uncomplicate.neanderthal.vect-math :refer :all]
             [uncomplicate.neanderthal.native :refer :all]))
 
+(def max-dim 2048)
+
+(def unit-vector (dv (replicate max-dim 1)))
+
+(defn prepare-unit-vector
+  "preparing unit vector for other calculations"
+  [n]
+  (if (<= n max-dim)
+    (subvector unit-vector 0 n)
+    (dv [0])))
+
 (defn random-number
   "random number in interval [0 .. 0.1]"
   [x]
@@ -16,5 +27,40 @@
 (defn layer-output
   [input weights o-func]
   (o-func (mv weights input)))
+
+(defn dtanh
+  [y]
+  (xpy (prepare-unit-vector (dim y)) (scal -1 (sqr (tanh y)))))
+
+(defn output-deltas
+  [targets outputs]
+  (let [error (axpy targets (scal -1 outputs))]
+    (mul (dtanh outputs) error)))
+
+
+(defn hidden-error
+  "output-deltas vector & copy weigths matrix & counter"
+  [o-deltas weights current]
+  (let [delta-scalar (entry o-deltas current)
+        neuron-weights (row weights current)]
+    (if (< current (dec (dim o-deltas)))
+      (do
+        (scal! delta-scalar neuron-weights)
+        (hidden-error o-deltas weights (inc current)))
+      (mv (trans weights) (prepare-unit-vector (inc current))))))
+
+(defn hidden-deltas
+  "hidden-error vector & hidden-output vector"
+  [h-error h-output]
+  (mul h-error (dtanh h-output)))
+
+(defn change-output-weights
+  [o-weights o-deltas o-hidden speed-learning current]
+  (let [o-scalar (entry o-deltas current)
+        neuron-weights (row o-weights current)]
+    (do
+      (axpy! (scal speed-learning (mul (dv (replicate (dim o-hidden) o-scalar)) o-hidden)) neuron-weights)
+      (if (< (inc current) (dim o-deltas))
+        (change-output-weights o-weights o-deltas o-hidden speed-learning (inc current))))))
 
 
